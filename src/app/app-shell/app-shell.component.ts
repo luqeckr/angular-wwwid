2523380e1ad4d03f13128b04665ca968be7eb133
Luqman <luqeckr@gmail.com>
2018-03-16 21:27:31 +0800

    app-shell component

---
+++ src/app/app-shell/app-shell.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shell',
+  templateUrl: './app-shell.component.html',
+  styleUrls: ['./app-shell.component.css']
+})
+export class AppShellComponent {
+
+  constructor() { }
+
+
+}
