e86bf9899b1eb307da3801457a049f3c0b552d16
Luqman <luqeckr@gmail.com>
2018-03-19 17:02:06 +0800

    detek scroll untuk lazyload image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import 'rxjs/add/operator/filter';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -13,11 +14,16 @@ export class AppComponent {
   item;
   bbut = false;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private data: DataService) {
     router.events.filter(e => e instanceof NavigationEnd).subscribe( nav => {
       console.log(nav);
       if (nav['url'] !== '/') { this.bbut = true; } else { this.bbut = false; }
     });
+  }
+  
+  scrollHandler(event: Event) {
+    // console.log(event);
+    this.data.loadImages();
   }
 
 }
