1956adf0c66a479e99287e5a3183450d7db4a84e
Luqman <luqeckr@gmail.com>
2018-04-09 08:03:39 +0800

    new update & scroll event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import 'rxjs/add/operator/filter';
 import { DataService } from './data.service';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
   items = [];
   item;
   kats = [];
   bbut = false;
   isKategori = false;
+  newUpdate = false;
+  updateMsg = '';
 
-  constructor(private router: Router, private data: DataService) {
+  constructor(private router: Router, private data: DataService, private swUpdate: SwUpdate) {
     router.events.filter(e => e instanceof NavigationEnd).subscribe( nav => {
       if (nav['url'] !== '/') { this.bbut = true; } else { this.bbut = false; }
     });
   }
+
+  ngOnInit() {
+    // this.swUpdate.checkForUpdate().then(a => {
+    //   console.log(a);
+    // }).catch(err => {
+    //   console.log(err);
+    // })
+    this.swUpdate.available.subscribe(event => {
+      // window.alert('[App] Update available: current version is '+ event.current.hash + ' available version is ' + event.available.hash);
+      // console.log(event);
+      this.newUpdate = true;
+      this.updateMsg = '[App] Versi Baru Tersedia';
+    })
+  }
   
   scrollHandler(event: Event) {
-    this.data.loadImages(); 
+    // this.data.loadImages(); 
+    this.data.emitScroll();
   }
 
   toggleKat() {
@@ -36,6 +54,10 @@ export class AppComponent {
   gotoKat(kat) {
     this.router.navigate(['cat', kat]);
     this.isKategori = false;
+  }
+
+  reload() {
+    location.reload();
   }
 
 }
